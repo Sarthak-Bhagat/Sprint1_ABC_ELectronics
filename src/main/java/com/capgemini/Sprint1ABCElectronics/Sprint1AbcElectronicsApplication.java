24c6877d10a2416552fab4ffb7b825e7117b52d6
Sarthak Bhagat <sarth.bhagat@gmail.com>
2022-09-26 11:01:14 +0530

    Initial Commit

---
+++ src/main/java/com/capgemini/Sprint1ABCElectronics/Sprint1AbcElectronicsApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.Sprint1ABCElectronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint1AbcElectronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint1AbcElectronicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/Sprint1ABCElectronics/Sprint1AbcElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.Sprint1ABCElectronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint1AbcElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
