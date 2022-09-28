85671daf9b3121e2764119e9b86533911557c9a6
Sarthak Bhagat <sarth.bhagat@gmail.com>
2022-09-28 12:55:45 +0530

    Update ComplaintRepo.java

--- src/main/java/com/capgemini/repositories/ComplaintRepo.java
+++ src/main/java/com/capgemini/repositories/ComplaintRepo.java
@@ -9,12 +9,15 @@ import com.capgemini.entities.Complaint;
 
 public interface ComplaintRepo extends JpaRepository<Complaint, Long> {
 
-	@Query(value = "Select * from Complaint where client_id = ?1", nativeQuery = true)
+//	@Query(value = "Select * from tbl_complaint where client_id = ?1", nativeQuery = true)
+	@Query("Select all c from Complaint c where c.client.clientId = ?1")
 	List<Complaint> findByClientId(long clientid);
 
-	@Query(value = "Select * from Complaint where employee_id = ?1", nativeQuery = true)
+//	@Query(value = "Select * from Complaint where employee_id = ?1", nativeQuery = true)
+	@Query("Select all c from Complaint c where c.engineer.employee_id = ?1")
 	List<Complaint> findByEmployeeId(long engineerId);
 
-	@Query(value = "Select * from Complaint where product_model_number = ?1", nativeQuery = true)
+//	@Query(value = "Select * from Complaint where product_model_number = ?1", nativeQuery = true)
+	@Query("Select all c from Complaint c where c.product.modelNumber = ?1")
 	List<Complaint> findByModelNumber(long modelNumber);
 }
