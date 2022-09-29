d8ff1a41e9b530f87639c6b5284f7897fd578261
Vidyamanohar <111647648+Vidyamanohar@users.noreply.github.com>
2022-09-29 11:53:33 +0530

    Update EngineerController.java

--- src/main/java/com/capgemini/controllers/EngineerController.java
+++ src/main/java/com/capgemini/controllers/EngineerController.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capgemini.entities.Complaint;
 import com.capgemini.entities.Engineer;
 import com.capgemini.exceptions.InvalidCredentialsException;
-import com.capgemini.extra.LoginCreds;
+import com.capgemini.extra.LoginDetails;
 import com.capgemini.services.EngineerService;
 
 @RestController
@@ -26,26 +28,26 @@ public class EngineerController {
 	@Autowired
 	EngineerService service;
 
-	@GetMapping("/all")
-	public List<Engineer> getAllEngineers() {
-		return service.getEngineers();
+	
+	private boolean checkSession(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		try {
+			LoginDetails currentUser = (LoginDetails) session.getAttribute("userDetails");
+			System.out.println(currentUser);
+			if (currentUser.isAdmin()) {
+				return true;
+			}
+			return false;
+		} catch (NullPointerException e) {
+			return false;
+		}
 	}
-
-	@GetMapping("/open/{engineerId}")
-	public List<Complaint> getAllOpenComplaints(@PathVariable long engineerId) {
-		return service.getAllOpenComplaints(engineerId);
-	}
-
-	@GetMapping("/resolved/{engineerId}")
-	public List<Complaint> getResolvedComplaints(@PathVariable long engineerId) {
-		return service.getResolvedComplaints(engineerId);
-	}
-
+	
 	@PostMapping("/signin")
-	public void signInWithCredentials(@RequestBody LoginCreds creds, HttpServletRequest request) {
+	public void signInWithCredentials(@RequestBody LoginDetails loginDetails, HttpServletRequest request) {
 
-		long username = creds.getUserId();
-		String password = creds.getPassword();
+		long username = loginDetails.getUserId();
+		String password =loginDetails.getPassword();
 
 		if (service.login(username, password)) {
 
@@ -57,5 +59,50 @@ public class EngineerController {
 		}
 
 	}
+	@GetMapping("/signout")
+	public ResponseEntity<String> signout(HttpServletRequest request) {
+		boolean validLogin = checkSession(request);
+
+		if (!validLogin) {
+			return new ResponseEntity<String>("USER NOT FOUND", HttpStatus.NOT_FOUND);
+		}
+
+		HttpSession session = request.getSession(true);
+		LoginDetails loginDetails = (LoginDetails) session.getAttribute("userDetails");
+		loginDetails.setAdmin(false);
+		session.setAttribute("userDetails", loginDetails);
+		return new ResponseEntity<String>("LOGGED IN", HttpStatus.FOUND);
+	}
+	@GetMapping("/all")
+	public List<Engineer> getAllEngineers(HttpServletRequest request) {
+		boolean validLogin = checkSession(request);
+
+		if (!validLogin) {
+			throw new InvalidCredentialsException();
+		}
+		return service.getEngineers();
+	}
+
+	@GetMapping("/open/{engineerId}")
+	public List<Complaint> getAllOpenComplaints(@PathVariable long engineerId, HttpServletRequest request) {
+		boolean validLogin = checkSession(request);
+
+		if (!validLogin) {
+			throw new InvalidCredentialsException();
+		}
+		return service.getAllOpenComplaints(engineerId);
+	}
+
+	@GetMapping("/resolved/{engineerId}")
+	public List<Complaint> getResolvedComplaints(@PathVariable long engineerId, HttpServletRequest request) {
+		boolean validLogin = checkSession(request);
+
+		if (!validLogin) {
+			throw new InvalidCredentialsException();
+		}
+		return service.getResolvedComplaints(engineerId);
+	}
+
+	
 
 }
