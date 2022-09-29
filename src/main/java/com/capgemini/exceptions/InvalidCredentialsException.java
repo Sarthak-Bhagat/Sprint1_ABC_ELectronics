c99e79b7e3fc72706d46fafd63aa73f70f0e8d42
Sarthak Bhagat <sarth.bhagat@gmail.com>
2022-09-29 10:58:29 +0530

    Updated Exceptions

--- src/main/java/com/capgemini/exceptions/InvalidCredentialsException.java
+++ src/main/java/com/capgemini/exceptions/InvalidCredentialsException.java
@@ -1,11 +1,15 @@
 package com.capgemini.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class InvalidCredentialsException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
 	public InvalidCredentialsException() {
-		super("Incorrect username or password");
+		super("Invalid Login Credentials, please login again");
 	}
 
 }
