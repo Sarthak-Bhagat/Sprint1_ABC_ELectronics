e6f29dc3ee75b6efebde05a810d47df0c84418c4
Vrushali Magdum <vrishi.magdum411@gmail.com>
2022-09-29 13:12:42 +0530

    Updated Product Service Implementation

--- src/main/java/com/capgemini/services/ProductServiceImpl.java
+++ src/main/java/com/capgemini/services/ProductServiceImpl.java
@@ -51,6 +51,8 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public List<Complaint> getProductComplaints(long modelNumber) {
+		productRepo.findById(modelNumber).orElseThrow(InvalidModelNumberException::new);
+		
 		return complaintRepo.findByProductModelNumber(modelNumber);
 
 	}
@@ -62,6 +64,8 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public void removeProduct(long modelNumber) {
+		
+		productRepo.findById(modelNumber).orElseThrow(InvalidModelNumberException::new);
 		productRepo.deleteById(modelNumber);
 	}
 
