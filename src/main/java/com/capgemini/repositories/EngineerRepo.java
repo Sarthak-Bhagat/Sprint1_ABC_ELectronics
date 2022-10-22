5b0f17384a0474a2256a99c46f3a9fcd5317d800
Dez <sarth.bhagat@gmail.com>
2022-10-22 10:43:14 +0530

    removed other files

--- src/main/java/com/capgemini/repositories/EngineerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.entities.Engineer;
-
-public interface EngineerRepo extends JpaRepository<Engineer, Long> {
-
-}
--- src/main/java/com/capgemini/repositories/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.entities.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}
